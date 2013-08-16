7c86eec1674f1e959d98f7683b20fe7be19d413c
wanderer <null_radix@riseup.net>
2013-08-16 11:57:24 -0700

    rebuiled

--- build/main.js
+++ build/main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var map;
+  var aboutTmp, map, markerArray;
   L.Icon.Default.imagePath = "bower_components/leaflet/dist/images";
   map = L.map('map', {
     zoomControl: false
@@ -11,6 +11,8 @@ window.onload = function() {
   map.addControl(new L.Control.Zoom({
     position: 'topright'
   }));
+  aboutTmp = window.JST["src/templates/about"]();
+  $("#about").html(aboutTmp);
   $("#topPanelWrapper").on('transitionend webkitTransitionEnd oTransitionEnd otransitionend', function() {
     if (!$(".panelWrapper").hasClass("open")) {
       return $(".panelWrapper").addClass("hidden");
@@ -18,29 +20,52 @@ window.onload = function() {
   });
   $(".closeButton").click(function() {
     $('#topPanelWrapper').height(80);
-    return $('#aboutWrapper').removeClass("open");
+    return $('.panelWrapper').removeClass("open");
   });
-  $.getJSON('data/about.json', function(data) {
-    $('#about').html(data);
-    return $("#aboutLink").click(function() {
-      if (!$(".panelWrapper").hasClass("open")) {
-        $('#aboutWrapper').removeClass("hidden").addClass("open");
-        return $('#topPanelWrapper').height(function(index, height) {
-          return height + $("#about").height() + 20;
-        });
-      } else {
-        return $('.closeButton').trigger('click');
-      }
-    });
+  $("nav a").click(function(event) {
+    var el;
+    el = $(event.currentTarget).data('el');
+    if (!$('#' + el + 'Wrapper').hasClass("open")) {
+      $('.panelWrapper').addClass('hidden').removeClass('open');
+      $('#' + el + 'Wrapper').removeClass("hidden").addClass("open");
+      return $('#topPanelWrapper').height(function(index, height) {
+        var fullHeight;
+        fullHeight = $(window).height() - 96;
+        if (fullHeight < $("#" + el).height() + 110) {
+          $('#' + el + 'Wrapper').height(fullHeight - 66).css("overflow-y", "scroll");
+          return fullHeight;
+        } else {
+          return $("#" + el).height() + 110;
+        }
+      });
+    } else {
+      return $('.closeButton').trigger('click');
+    }
   });
+  markerArray = [];
   return $.getJSON('data/map.geojson', function(data) {
-    var gjLayer;
+    var gjLayer, listTmp;
     gjLayer = L.geoJson(data, {
       onEachFeature: function(feature, layer) {
         var popupTmp;
         popupTmp = window.JST["src/templates/popup"](feature.properties);
-        return layer.bindPopup(popupTmp);
+        markerArray.push({
+          maker: layer.bindPopup(popupTmp),
+          data: feature.properties
+        });
+        return markerArray.sort(function(a, b) {
+          return a.data.Name.localeCompare(b.data.Name);
+        });
       }
+    });
+    listTmp = window.JST["src/templates/list"]({
+      "markerArray": markerArray
+    });
+    $("#list").html(listTmp);
+    $(".listLink").click(function(event) {
+      var index;
+      index = $(event.currentTarget).data('index');
+      return markerArray[index].maker.openPopup();
     });
     return gjLayer.addTo(map);
   });
--- build/tmp.js
+++ build/tmp.js
@@ -1,5 +1,150 @@
 this["JST"] = this["JST"] || {};
 
+this["JST"]["src/templates/about"] = function anonymous(locals
+/**/) {
+jade.debug = [{ lineno: 1, filename: "src/templates/about.jade" }];
+try {
+var buf = [];
+jade.debug.unshift({ lineno: 1, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 1, filename: jade.debug[0].filename });
+buf.push("The Detroit farm map is an open source collaboration project to map all the urban farms and gardens in the Detroit metro area.");
+jade.debug.shift();
+buf.push("\n");
+jade.debug.unshift({ lineno: 2, filename: jade.debug[0].filename });
+buf.push("If you know of a farm or garden that needs to be on this map or have questions/comments shoot us an email at ");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 3, filename: jade.debug[0].filename });
+buf.push("<a href=\"mailto:info@farmmap.us\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 3, filename: jade.debug[0].filename });
+buf.push("info@farmmap.us");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</a>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 5, filename: jade.debug[0].filename });
+buf.push("<hr/>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 5, filename: jade.debug[0].filename });
+buf.push("If you are interested in setting up a similar map in your area let us know, we would love to help out!  ");
+jade.debug.shift();
+buf.push("\n");
+jade.debug.unshift({ lineno: 6, filename: jade.debug[0].filename });
+buf.push("Source code is avaible ");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 7, filename: jade.debug[0].filename });
+buf.push("<a href=\"https://github.com/wanderer/Detroit-Farm-Map\" target=\"_blank\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 7, filename: jade.debug[0].filename });
+buf.push("here");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</a>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 9, filename: jade.debug[0].filename });
+buf.push("<hr/>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 9, filename: jade.debug[0].filename });
+buf.push("<a href=\"bitcoin:1LW5fuyfrwwpXeVDFVf6eHr95ei79bFktF?label=detroit%20farm%20map\">");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 9, filename: jade.debug[0].filename });
+buf.push("Click here to donate with Bitcoins  ");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</a>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 11, filename: jade.debug[0].filename });
+buf.push("<br/>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 11, filename: jade.debug[0].filename });
+buf.push("If clicking on the line above does not work, use this address:");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 13, filename: jade.debug[0].filename });
+buf.push("<br/>");
+jade.debug.shift();
+jade.debug.unshift({ lineno: 13, filename: jade.debug[0].filename });
+buf.push("<code>");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 13, filename: jade.debug[0].filename });
+buf.push("1LW5fuyfrwwpXeVDFVf6eHr95ei79bFktF");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</code>");
+jade.debug.shift();
+jade.debug.shift();;return buf.join("");
+} catch (err) {
+  jade.rethrow(err, jade.debug[0].filename, jade.debug[0].lineno);
+}
+};
+
+this["JST"]["src/templates/list"] = function anonymous(locals
+/**/) {
+jade.debug = [{ lineno: 1, filename: "src/templates/list.jade" }];
+try {
+var buf = [];
+var locals_ = (locals || {}),markerArray = locals_.markerArray;jade.debug.unshift({ lineno: 1, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 2, filename: jade.debug[0].filename });
+buf.push("<ul>");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 2, filename: jade.debug[0].filename });
+// iterate markerArray
+;(function(){
+  var $$obj = markerArray;
+  if ('number' == typeof $$obj.length) {
+
+    for (var i = 0, $$l = $$obj.length; i < $$l; i++) {
+      var data = $$obj[i];
+
+jade.debug.unshift({ lineno: 2, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 4, filename: jade.debug[0].filename });
+buf.push("<li>");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 4, filename: jade.debug[0].filename });
+buf.push("<a" + (jade.attrs({ 'href':("#"), 'data-index':(i), "class": [('listLink')] }, {"href":true,"data-index":true})) + ">" + (jade.escape(null == (jade.interp = data.data.Name) ? "" : jade.interp)));
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.shift();
+buf.push("</a>");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</li>");
+jade.debug.shift();
+jade.debug.shift();
+    }
+
+  } else {
+    var $$l = 0;
+    for (var i in $$obj) {
+      $$l++;      var data = $$obj[i];
+
+jade.debug.unshift({ lineno: 2, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 4, filename: jade.debug[0].filename });
+buf.push("<li>");
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.unshift({ lineno: 4, filename: jade.debug[0].filename });
+buf.push("<a" + (jade.attrs({ 'href':("#"), 'data-index':(i), "class": [('listLink')] }, {"href":true,"data-index":true})) + ">" + (jade.escape(null == (jade.interp = data.data.Name) ? "" : jade.interp)));
+jade.debug.unshift({ lineno: undefined, filename: jade.debug[0].filename });
+jade.debug.shift();
+buf.push("</a>");
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</li>");
+jade.debug.shift();
+jade.debug.shift();
+    }
+
+  }
+}).call(this);
+
+jade.debug.shift();
+jade.debug.shift();
+buf.push("</ul>");
+jade.debug.shift();
+jade.debug.shift();;return buf.join("");
+} catch (err) {
+  jade.rethrow(err, jade.debug[0].filename, jade.debug[0].lineno);
+}
+};
+
 this["JST"]["src/templates/popup"] = function anonymous(locals
 /**/) {
 jade.debug = [{ lineno: 1, filename: "src/templates/popup.jade" }];
